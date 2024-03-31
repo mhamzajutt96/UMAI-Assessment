7cf4478a88a964f4a1e010f24fb7a148afb66a4d
Muhammad Hamza <mhamzajutt96@gmail.com>
2024-03-31 21:44:10 +0500

    Use bulk insertion to optimize seeds performance.

--- seeds.rb
+++ seeds.rb
@@ -1,6 +1,7 @@
 puts 'Seeds Execution is starting!'
 
 NUM_POSTS = 200_000
+POSTS_CHUNK_SIZE = 13_000
 NUM_USERS = 100
 NUM_IPS = 50
 
@@ -32,6 +33,7 @@ def generate_random_post_ids(num_posts)
 end
 
 def generate_post_feedback(num_feedback, user_ids, post_ids)
+  values = []
   feedback_tracker = Hash.new { |hash, key| hash[key] = Set.new }
   num_feedback.times do
     post_id = post_ids.sample
@@ -39,12 +41,15 @@ def generate_post_feedback(num_feedback, user_ids, post_ids)
     owner_id = user_ids.sample
     unless feedback_tracker[post_id].include?(owner_id)
       feedback_tracker[post_id].add(owner_id)
-      system("mysql -u#{DB_USERNAME} -h#{DB_HOST} -e \"USE #{DB_NAME}; INSERT INTO feedbacks (post_id, comment, owner_id) VALUES ('#{post_id}', '#{comment}', '#{owner_id}');\"")
+      values << [post_id, comment, owner_id]
     end
   end
+  columns = %w[post_id comment owner_id]
+  bulk_insert('feedbacks', columns, values)
 end
 
 def generate_user_feedback(num_feedback, user_ids)
+  values = []
   feedback_tracker = Hash.new { |hash, key| hash[key] = Set.new }
   num_feedback.times do
     user_id = user_ids.sample
@@ -52,32 +57,58 @@ def generate_user_feedback(num_feedback, user_ids)
     owner_id = user_ids.sample
     unless feedback_tracker[user_id].include?(owner_id)
       feedback_tracker[user_id].add(owner_id)
-      system("mysql -u#{DB_USERNAME} -h#{DB_HOST} -e \"USE #{DB_NAME}; INSERT INTO feedbacks (user_id, comment, owner_id) VALUES ('#{user_id}', '#{comment}', '#{owner_id}');\"")
+      values << [user_id, comment, owner_id]
     end
   end
+  columns = %w[user_id comment owner_id]
+  bulk_insert('feedbacks', columns, values)
+end
+
+def bulk_insert(table, columns, values)
+  columns_str = columns.join(', ')
+  case table
+  when 'users'
+    values_str = values.map { |row| "(#{row.join(', ')})" }.join(', ')
+  else
+    values_str = values.map do |row|
+      "(#{row.map.with_index { |value, index| integer_column?(columns[index]) ? value : "'#{value}'" }.join(', ')})"
+    end.join(', ')
+  end
+  query = "INSERT INTO #{table} (#{columns_str}) VALUES #{values_str};"
+  system("mysql -u #{DB_USERNAME} -h #{DB_HOST} -e \"USE #{DB_NAME}; #{query}\"")
+end
+
+def integer_column?(column_name)
+  column_name.end_with?('_id')
 end
 
 ips = generate_random_ips(NUM_IPS)
 
-NUM_USERS.times do
-  email = generate_random_email
-  system("mysql -u#{DB_USERNAME} -h#{DB_HOST} -e \"USE #{DB_NAME}; INSERT INTO users (email) VALUES ('#{email}');\"")
-end
+emails = (1..NUM_USERS).map { generate_random_email }
+columns = ['email']
+values = emails.map { |email| ["'#{email}'"] }
+bulk_insert('users', columns, values)
 puts 'Users seeds data generated successfully.'
 
-NUM_POSTS.times do
+posts = (1..NUM_POSTS).map do
   title = generate_random_text(10)
   content = generate_random_text(100)
-  author_id = (1..NUM_USERS).to_a.sample
+  author_id = rand(1..NUM_USERS)
   ip_address = ips.sample
   author_login = generate_random_text(8)
-  system("mysql -u#{DB_USERNAME} -h#{DB_HOST} -e \"USE #{DB_NAME}; INSERT INTO posts (title, content, author_id, author_ip, author_login) VALUES ('#{title}', '#{content}', #{author_id}, '#{ip_address}', '#{author_login}');\"")
+  [title, content, author_id, ip_address, author_login]
+end
+
+columns = %w[title content author_id author_ip author_login]
+post_chunks = posts.each_slice(POSTS_CHUNK_SIZE).to_a
+post_chunks.each do |chunk|
+  bulk_insert('posts', columns, chunk)
 end
 puts 'Posts seeds data generated successfully.'
 
 user_ids = generate_random_user_ids(NUM_USERS)
 post_ids = generate_random_post_ids(NUM_POSTS)
-
+#
 generate_post_feedback(NUM_POST_FEEDBACK, user_ids, post_ids)
 puts 'Posts feedbacks seeds data generated successfully.'
 
